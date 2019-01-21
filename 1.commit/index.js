561db4ffa2e5799593aaa3e1d683bca1b4bc004a
HQuaan <lvhquan0110@gmail.com>
2019-01-21 19:11:20 +0700

    Improve cat eat method

--- 1.commit/index.js
+++ 1.commit/index.js
@@ -6,5 +6,12 @@ var dog = new Dog('Tom');
 var cat = new Cat();
 var mouse = new Mouse('Jerry', 10)
 
+try {
+    cat.eat(dog);
+} catch (error) {
+    console.log('Error while cat eating');
+}
+
+console.log(cat)
 dog.eat(cat);
 console.log(dog);
