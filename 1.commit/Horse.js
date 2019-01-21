6560613a2765b2e2ded938a53677c62c1fd5658d
HQuaan <lvhquan0110@gmail.com>
2019-01-21 20:36:39 +0700

    Horse run method

--- 1.commit/Horse.js
+++ 1.commit/Horse.js
@@ -2,7 +2,11 @@ function Horse() {
 }
 
 Horse.prototype.jump = function(){
-	console.log('jump');
+    console.log('jump');
+}
+
+Horse.prototype.run = function () {
+    console.log('Runnig');
 }
 
 module.exports = Horse;
